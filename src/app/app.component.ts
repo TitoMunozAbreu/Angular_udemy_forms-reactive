cd6d71d1855ab838f3b0edba2ac6714147e2a805
titomunoz <munoztitojose@gmail.com>
2023-12-05 07:47:55 +0100

    Project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forms-reactive-assignment';
+  projectStatus = ['Stable', 'Critical', 'Finished']
+  projectForm !: FormGroup
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.projectForm = this.fb.group({
+      name: ['', Validators.required],
+      mail: ['', Validators.required, Validators.email]
+    })
+  }
+
+
 }
